e0a831a811a968c920585fff2be28de12b07663b
haybuka <paschal.nduleo@gmail.com>
2023-07-10 13:24:12 +0100

    login setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import Select from './components/select';
 import LoginForm from './components/form';
+import { useState } from 'react';
+
+type AccountType = { name: string; value: string; img: string; id: number };
 
 function App() {
+  const [accountName, setAccountName] = useState<AccountType | undefined>({
+    name: 'arya',
+    value: 'arya',
+    id: 1,
+    img: '',
+  });
+
+  const handleAccountChange = (profile: AccountType | undefined) => {
+    setAccountName(profile);
+  };
   return (
     <main className="w-screen h-screen overflow-hidden flex justify-center items-center">
       <section className="w-[380px] shadow-lg bg-white p-2 rounded-lg">
-        <Select />
+        <Select accountChange={handleAccountChange} />
         <p className="bg-gray-200 h-24 w-24 rounded-full mx-auto my-6"></p>
         <h3 className="text-center">
           <p>Welcome back.</p>
-          <p className="font-semibold">Arya</p>
+          <p className="font-semibold capitalize">{accountName?.value}</p>
         </h3>
         <LoginForm />
         <p className="text-center text-blue-800 my-3">unable to log in?</p>
--- src/components/select.tsx
+++ src/components/select.tsx
@@ -1,17 +1,61 @@
-import React from 'react';
+import { ChangeEvent } from 'react';
 
-const Select = () => {
+interface Props {
+  accountChange: (person: AccountType | undefined) => void;
+}
+
+type SelectChange = ChangeEvent<HTMLSelectElement>;
+type AccountType = { name: string; value: string; img: string; id: number };
+
+const Select = ({ accountChange }: Props) => {
+  const accountData = [
+    {
+      name: 'Change Account',
+      value: 'arya',
+      img: '',
+      id: 1,
+    },
+    {
+      name: 'The hound',
+      value: 'the hound',
+      img: '',
+      id: 2,
+    },
+    {
+      name: 'cersei',
+      value: 'cersei',
+      img: '',
+      id: 3,
+    },
+    {
+      name: 'jon snow',
+      value: 'jon snow',
+      img: '',
+      id: 4,
+    },
+  ];
+  const selectChange = (event: SelectChange) => {
+    const accountId = event.target.value;
+    const profile = accountData?.find(
+      (account) => account?.id === Number(accountId)
+    );
+
+    accountChange(profile);
+  };
+
   return (
     <div className="flex justify-end">
       <select
         name="userType"
         id="userType"
         className="p-2 my-2 outline-none border-0 capitalize"
+        onChange={selectChange}
       >
-        <option value="">change account</option>
-        <option value="">the hound</option>
-        <option value="">cersei</option>
-        <option value="">cerseiJohn snow</option>
+        {accountData.map((account) => (
+          <option value={account.id} key={account.id}>
+            {account.name}
+          </option>
+        ))}
       </select>
     </div>
   );
